7ebb21b4c08a3e8500d2f84959822ed25688c8a5
Olivier Le Rigoleur <olivier.le.rigoleur@accenture.com>
2019-01-29 11:17:35 +0100

    Update mainapp.js

--- mainapp.js
+++ mainapp.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 app.get('/', function(req, res){
-  res.send('hello ' + req.query.id + ' and welcome to Dublin');
+  res.send('hello ' + req.query.id + ' and welcome to ICW Dublin');
 });
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
