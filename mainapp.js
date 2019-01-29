92d1ed3bd7d3bacaef5a2dce9a56644b4870ccbb
Olivier Le Rigoleur <olivier.le.rigoleur@accenture.com>
2019-01-29 10:49:14 +0100

    move to express.js

--- mainapp.js
+++ mainapp.js
@@ -1,23 +1,6 @@
-// content of index.js
-const http = require('http')
-const port = 3000
-
-const requestHandler = (request, response) => {
-  console.log(request.url)
-  var url = require('url');
-  var url_parts = url.parse(request.url, true);
-  var query = url_parts.query;
-  var id = query.id;
-  response.end('Hello $(id) from Dublin!')
-// comment for futur devs
-}
-
-const server = http.createServer(requestHandler)
-
-server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
-})
+const express = require('express')
+const app = express()
+app.get('/', function(req, res){
+  res.send('hello ' + req.query.id + ' and welcome to Dublin');
+});
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
