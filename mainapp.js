cc689f016d375252744f5017a58c2a0e0012ea8b
Olivier Le Rigoleur <olivier.le.rigoleur@accenture.com>
2019-01-29 21:39:03 +0100

    Update mainapp.js

--- mainapp.js
+++ mainapp.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const app = express()
 app.get('/', function(req, res){
-  if(req.query.id !== undefined){
-    res.send('hello ' + req.query.id + ' and welcome to ICW   Dublin');
-  }
-  else{res.send('hello and welcome to H&PS CIW in Dublin');}
+  res.send('hello ' + req.query.id + ' and welcome to ICW Dublin 2019');
 });
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
